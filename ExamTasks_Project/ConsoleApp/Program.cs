8de5baa9776e686bd81269daee3baf499564f07e
Dieselholic <fgjbffubffyhv@gmail.com>
2021-07-03 22:03:54 +0300

    Update Program.cs

--- ExamTasks_Project/ConsoleApp/Program.cs
+++ ExamTasks_Project/ConsoleApp/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using ConsoleApp.Tasks;
 
 namespace ConsoleApp
@@ -11,61 +8,61 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
 
-            //RunFunctionWithName(Task1.Run);
+            RunFunctionWithName(Task1.Run);
 
-            //RunFunctionWithName(Task2.Run);
+            RunFunctionWithName(Task2.Run);
 
-            //RunFunctionWithName(Task3.Run);
+            RunFunctionWithName(Task3.Run);
 
-            //RunFunctionWithName(Task4.Run);
+            RunFunctionWithName(Task4.Run);
 
-            //RunFunctionWithName(Task5.Run);
+            RunFunctionWithName(Task5.Run);
 
-            //RunFunctionWithName(Task6.Run);
+            RunFunctionWithName(Task6.Run);
 
-            //RunFunctionWithName(Task7.Run);
+            RunFunctionWithName(Task7.Run);
 
-            //RunFunctionWithName(Task8.Run);
+            RunFunctionWithName(Task8.Run);
 
-            //RunFunctionWithName(Task9.Run);
+            RunFunctionWithName(Task9.Run);
 
-            //RunFunctionWithName(Task10.Run);
+            RunFunctionWithName(Task10.Run);
 
-            //RunFunctionWithName(Task11.Run);
+            RunFunctionWithName(Task11.Run);
 
-            //RunFunctionWithName(Task12.Run);
+            RunFunctionWithName(Task12.Run);
 
-            //RunFunctionWithName(Task13.Run);
+            RunFunctionWithName(Task13.Run);
 
-            //RunFunctionWithName(Task14.Run);
+            RunFunctionWithName(Task14.Run);
 
-            //RunFunctionWithName(Task15.Run);
+            RunFunctionWithName(Task15.Run);
 
-            //RunFunctionWithName(Task16.Run);
+            RunFunctionWithName(Task16.Run);
 
-            //RunFunctionWithName(Task17.Run);
+            RunFunctionWithName(Task17.Run);
 
-            //RunFunctionWithName(Task18.Run);
+            RunFunctionWithName(Task18.Run);
 
-            //RunFunctionWithName(Task19.Run);
+            RunFunctionWithName(Task19.Run);
 
-            //RunFunctionWithName(Task20.Run);
+            RunFunctionWithName(Task20.Run);
 
-            //RunFunctionWithName(Task21.Run);
+            RunFunctionWithName(Task21.Run);
 
-            //RunFunctionWithName(Task22.Run);
+            RunFunctionWithName(Task22.Run);
 
-            //RunFunctionWithName(Task23.Run);
+            RunFunctionWithName(Task23.Run);
 
             RunFunctionWithName(Task25.Run);
 
-            //RunFunctionWithName(Task26.Run);
+            RunFunctionWithName(Task26.Run);
 
-            //RunFunctionWithName(Task27.Run);
+            RunFunctionWithName(Task27.Run);
 
-            //RunFunctionWithName(Task28.Run);
+            RunFunctionWithName(Task28.Run);
 
-            //RunFunctionWithName(Task29.Run);
+            RunFunctionWithName(Task29.Run);
 
             Console.ReadLine();
         }
